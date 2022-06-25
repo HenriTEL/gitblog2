132d5ba41a97d8e6a9b43d9d839c16ef3076c260
HenriTEL <henri.hannetel@pm.me>
2022-06-25 18:35:51 +0100

    Fixed templates path

--- gen_blog.sh
+++ gen_blog.sh
@@ -4,7 +4,7 @@ OUTPUT_DIR=$1
 
 for md_article_path in $(find articles/ -name '*.md'); do
 	html_article_path="${OUTPUT_DIR}/${md_article_path%%.*}.html"
-	gomplate --file "templating/article.tmpl.html" \
+	gomplate --file "templates/article.tmpl.html" \
 	    --datasource article_md="file://${PWD}/${input_article_path}" \
 		--out "${html_article_path}"
 done
