1f82c7744cf9d59f3e6d3695c5995ae4372c279f
HenriTEL <henri.hannetel@pm.me>
2022-06-26 20:17:29 +0100

    Added Open graph tags, favicon and other stuff

--- gen_blog.sh
+++ gen_blog.sh
@@ -2,9 +2,10 @@
 set -e
 OUTPUT_DIR=$1
 
+cp -r media/ "${OUTPUT_DIR}"
 for md_article_path in $(find articles/ -name '*.md'); do
 	html_article_path="${OUTPUT_DIR}/${md_article_path%%.*}.html"
 	gomplate --file "templates/article.tmpl.html" \
-		--datasource article_md="file://${PWD}/${md_article_path}" \
+		--context article_md="file://${PWD}/${md_article_path}" \
 		--out "${html_article_path}"
 done
