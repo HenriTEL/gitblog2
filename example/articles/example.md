8a28be0d6c65eda54f263028567a8c4298bb106d
HenriTEL <henri.hannetel@pm.me>
2022-07-23 17:44:51 +0100

    Update example content

---
+++ example/articles/example.md
@@ -0,0 +1,10 @@
+# Hello World
+
+**TL;DR** This article was made using git-blog.  
+
+```python
+def foo():
+    return "bar"
+```
+
+Some **bold** *italic* ***both*** formatting.  
--- git-blog.sh
+++ git-blog.sh
@@ -3,9 +3,9 @@ set -e
 SOURCE_DIR="/blog"
 TARGET_DIR="/out"
 
-[ -d "${TARGET_DIR}" ] || mkdir -p "${TARGET_DIR}"
+mkdir -p "${TARGET_DIR}"
 for md_article in $(find "${SOURCE_DIR}/articles" -name '*.md'); do
-	html_article="${TARGET_DIR}/${md_article%%.*}"
+	html_article="${TARGET_DIR}/${md_article%%.*}.html"
 	# git log formatting explained at https://git-scm.com/docs/pretty-formats
 	git -C "${SOURCE_DIR}" log --pretty=format:'%as|%aI|%an|%s' "${md_article}" \
 	| gomplate --file "templates/article.tmpl.html" \
