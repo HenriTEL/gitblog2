657203cd7da8141924a657e048975029bc1c97bc
Henri Hannetel <henri.hannetel@pm.me>
2022-11-16 20:57:14 +0000

    Render section indexes

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ from utils import listenv
 
 TEMPLATES_DIR_DEFAULT = "templates"
 REPO_URL = os.getenv("REPO_URL", "https://codeberg.org/HenriTEL/git-blog.git")
-REPO_SUBDIR = os.getenv("REPO_SUBDIR", "example/").lstrip("/").rstrip("/")
-REPO_TEMPLATES_DIR = os.getenv("REPO_TEMPLATES_DIR", TEMPLATES_DIR_DEFAULT).lstrip("/").rstrip("/")
+REPO_SUBDIR = os.getenv("REPO_SUBDIR", "").strip("/")
+REPO_TEMPLATES_DIR = os.getenv("REPO_TEMPLATES_DIR", TEMPLATES_DIR_DEFAULT).strip("/")
 REPO_DIRS_BLACKLIST = listenv("REPO_DIRS_BLACKLIST", ["draft", "media", "templates"])
 REPO_FILES_BLACKLIST = listenv("REPO_FILES_BLACKLIST", ["README.md"])
 CLONE_PATH = os.getenv("CLONE_PATH", "").rstrip("/")
@@ -32,81 +32,159 @@ regex sub
 
 def main():
     repo = setup_repo()
-    md_to_commits = get_commits_per_md(repo)
-    sections_to_md = get_sections_to_md(md_to_commits)
-    last_commit = repo[repo.head.target]
-
     # TODO link missing template files in REPO
     # templates_fulldir = f"{CLONE_PATH}/{REPO_TEMPLATES_DIR}"
     # if not os.path.exists(templates_fulldir):
     templates_fulldir = TEMPLATES_DIR_DEFAULT
     j2_env = jinja2.Environment(loader=jinja2.FileSystemLoader(templates_fulldir))
+
+    sections = list(gen_sections(repo))
+    section_to_paths = defaultdict(list)
+    path_to_article = defaultdict(dict)
+    for path, commit in gen_commits(repo):
+        if "commits" in path_to_article[path]:
+            path_to_article[path]["commits"].append(commit)
+        else:
+            path_to_article[path]["commits"] = [commit]
+        if "/" in path:
+            section = path.split("/")[0]
+            if section in sections:
+                section_to_paths[section].append(path)
+
+    last_commit = repo[repo.head.target]
+    for path, content in gen_articles_content(last_commit.tree):
+        print(path)
+        title, description, md_content = parse_md(content)
+        path_to_article[path]["title"] = title
+        path_to_article[path]["description"] = description
+        path_to_article[path]["relative_path"] = path[:-3]
+        render_article(
+            metadata=path_to_article[path],
+            md_content=content,
+            target_path=f"{OUTPUT_DIR}/{path.replace('.md', '.html')}",
+            sections=sections,
+            j2_env=j2_env,
+        )
+    for section in sections:
+        render_index(
+            articles_paths=section_to_paths[section],
+            path_to_article=path_to_article,
+            target_path=f"{OUTPUT_DIR}/{section}/index.html",
+            sections=sections,
+            j2_env=j2_env,
+        )
+
+
+def setup_repo() -> pygit2.Repository:
+    global CLONE_PATH, REPO_URL
+    if CLONE_PATH and os.path.exists(f"{CLONE_PATH}/.git/"):
+        repo = pygit2.Repository(CLONE_PATH)
+    else:
+        if not CLONE_PATH:
+            CLONE_PATH = mkdtemp()
+        os.makedirs(CLONE_PATH, exist_ok=True)
+        repo = pygit2.clone_repository(REPO_URL, CLONE_PATH)
+        logging.info(f"Cloned into {CLONE_PATH}")
+    return repo
+
+
+def render_article(
+    metadata: Dict[str, Any],
+    md_content: str,
+    target_path: str,
+    sections: List[str],
+    j2_env: jinja2.Environment,
+):
     template = j2_env.get_template("article.html.j2")
-    md_to_td = make_articles(last_commit, md_to_commits, sections_to_md, template)
+    os.makedirs(os.path.dirname(target_path), exist_ok=True)
+    html_content = markdown(md_content, extensions=MD_LIB_EXTENSIONS)
+    full_page = template.render(
+        title=metadata["title"],
+        description=metadata["description"],
+        main_content=html_content,
+        commits=metadata["commits"],
+        sections=sections,
+    )
+    print(full_page)
+    with open(target_path, "w+") as fd:
+        fd.write(full_page)
+    logging.info(f"{target_path} has been written.")
 
+
+def render_index(
+    articles_paths: List[str],
+    path_to_article: Dict[str, Dict[str, Any]],
+    target_path: str,
+    sections: List[str],
+    j2_env: jinja2.Environment,
+):
     template = j2_env.get_template("index.html.j2")
-    make_indexes(sections_to_md, md_to_commits, md_to_td, template)
+    os.makedirs(os.path.dirname(target_path), exist_ok=True)
+    articles = [path_to_article[p] for p in articles_paths]
+    full_page = template.render(
+        title=articles_paths[0].split("/")[0],
+        articles=articles,
+        sections=sections,
+    )
+    print(full_page)
+    with open(target_path, "w+") as fd:
+        fd.write(full_page)
+    logging.info(f"{target_path} has been written.")
 
 
-def make_articles(
-    last_commit: pygit2.Object,
-    md_to_commits: Dict[str, List[pygit2.Object]],
-    sections_to_md: Dict[str, List[str]],
-    template: jinja2.Template,
-) -> Dict[str, Tuple[str, str]]:
-    md_to_td = {}
-    for path, blob in gen_md_blobs(last_commit.tree):
-        base_name = f"{OUTPUT_DIR}/{path}"
-        html_path = base_name + blob.name.replace(".md", ".html")
-        os.makedirs(os.path.dirname(base_name), exist_ok=True)
-        commits = md_to_commits[path + blob.name]
-        title, desc, md_content = parse_md(blob.data.decode("utf-8"))
-        html_content = markdown(md_content, extensions=MD_LIB_EXTENSIONS)
-        full_page = template.render(
-            title=title,
-            description=desc,
-            main_content=html_content,
-            commits=commits,
-            sections=list(sections_to_md.keys()),
-        )
-        print(full_page)
-        with open(html_path, "w+") as fd:
-            fd.write(full_page)
-        logging.info(f"Updated {html_path}")
-        md_to_td[path + blob.name] = (title, desc)
-
-
-def make_indexes(sections_to_md, md_to_commits, md_to_td, template):
-        full_page = template.render(
-            title=title,
-            description=desc,
-            main_content=html_content,
-            commits=commits,
-            sections=list(sections_to_md.keys()),
-        )
-
-
-def get_commits_per_md(repo: pygit2.Repository):
-    def commit_to_dict(commit: pygit2.Commit) -> Dict[str, Any]:
+def gen_commits(repo: pygit2.Repository) -> Tuple[str, Dict[str, Any]]:
+    def clean_ci(commit: pygit2.Commit) -> Dict[str, Any]:
+        commit_dt = datetime.fromtimestamp(commit.commit_time)
         return {
-            attr: getattr(commit, attr) for attr in dir(commit) if not attr[0] == "_"
+            "iso_time": commit_dt.isoformat(),
+            "human_time": commit_dt.strftime("%d %B %Y"),
+            "author": commit.author,
+            "message": commit.message,
         }
 
-    md_to_commits = defaultdict(list)
     for commit in repo.walk(repo.head.target):
-        commit = commit_to_dict(commit)
-        if commit["parents"]:
-            prev = commit["parents"][0]
-            diff = prev.tree.diff_to_tree(commit["tree"])
+        if commit.parents:
+            prev = commit.parents[0]
+            diff = prev.tree.diff_to_tree(commit.tree)
             for patch in diff:
                 path = patch.delta.new_file.path
                 if path.endswith(".md") and path.startswith(f"{REPO_SUBDIR}/"):
                     path = path.removeprefix(f"{REPO_SUBDIR}/")
-                    commit_dt = datetime.fromtimestamp(commit["commit_time"])
-                    commit["iso_time"] = commit_dt.isoformat()
-                    commit["human_time"] = commit_dt.strftime("%d %B %Y")
-                    md_to_commits[path].append(commit)
-    return md_to_commits
+                    yield path, clean_ci(commit)
+
+
+def gen_articles_content(
+    tree: pygit2.Tree, path=""
+) -> Generator[Tuple[str, str], None, None]:
+    for obj in tree:
+        if obj.type == pygit2.GIT_OBJ_TREE and obj.name not in REPO_DIRS_BLACKLIST:
+            path += f"{obj.name}/"
+            yield from gen_articles_content(obj, path)
+
+        elif (
+            obj.name.endswith(".md")
+            and path.startswith(f"{REPO_SUBDIR}/")
+            and obj.name not in REPO_FILES_BLACKLIST
+        ):
+            path = path.removeprefix(f"{REPO_SUBDIR}/")
+            yield (path + obj.name, obj.data.decode("utf-8"))
+
+
+def gen_sections(repo: pygit2.Repository):
+    tree = repo[repo.head.target].tree
+    # Move to the REPO_SUBDIR location
+    for to_match in REPO_SUBDIR.split("/"):
+        for obj in tree:
+            if obj.type == pygit2.GIT_OBJ_TREE and obj.name == to_match:
+                tree = obj
+                break
+        if obj.name != to_match:
+            return
+
+    # Enumerate all valid toplevel dirs
+    for obj in tree:
+        if obj.type == pygit2.GIT_OBJ_TREE and obj.name not in REPO_DIRS_BLACKLIST:
+            yield obj.name
 
 
 def parse_md(md_content: str) -> Tuple[str, str, str]:
@@ -125,43 +203,15 @@ def parse_md(md_content: str) -> Tuple[str, str, str]:
     return title, desc, md_content
 
 
-def gen_md_blobs(
-    tree: pygit2.Tree, path=""
-) -> Generator[Tuple[str, pygit2.Object], None, None]:
-    for obj in tree:
-        if obj.type == pygit2.GIT_OBJ_TREE and obj.name not in REPO_DIRS_BLACKLIST:
-            path += f"{obj.name}/"
-            yield from gen_md_blobs(obj, path)
-
-        elif (
-            obj.name.endswith(".md")
-            and path.startswith(f"{REPO_SUBDIR}/")
-            and obj.name not in REPO_FILES_BLACKLIST
-        ):
-            path = path.removeprefix(f"{REPO_SUBDIR}/")
-            yield (path, obj)
-
-
-def get_sections_to_md(md_to_commits: Dict[str, List[pygit2.Object]]) -> Dict[str, List[str]]:
+def get_sections_to_md(
+    md_to_commits: Dict[str, List[pygit2.Object]]
+) -> Dict[str, List[str]]:
     sections_to_md = defaultdict(list)
     for path in md_to_commits.keys():
         if "/" in path:
             section = path.split("/")[0]
             sections_to_md[section].append(path)
     return sections_to_md
-
-
-def setup_repo() -> pygit2.Repository:
-    global CLONE_PATH, REPO_URL
-    if CLONE_PATH and os.path.exists(f"{CLONE_PATH}/.git/"):
-        repo = pygit2.Repository(CLONE_PATH)
-    else:
-        if not CLONE_PATH:
-            CLONE_PATH = mkdtemp()
-        os.makedirs(CLONE_PATH, exist_ok=True)
-        repo = pygit2.clone_repository(REPO_URL, CLONE_PATH)
-        logging.info(f"Cloned into {CLONE_PATH}")
-    return repo
 
 
 if __name__ == "__main__":
