98cdd10cf97ce6ba27943f55e8f7ef1d76fe4ecc
Henri Hannetel <henri.hannetel@pm.me>
2024-04-30 23:10:14 +0200

    Fixed readme not found

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM python:3.11-slim
+FROM python:3.12-slim
 
 ARG GITBLOG2_VERSION
 
 # ADD https://deb.nodesource.com/setup_16.x /tmp/setup_nodejs_16
 # RUN bash /tmp/setup_nodejs_16
-RUN apt update \
-	&& apt install -y zip git
+RUN apt-get update \
+	&& apt-get install -y zip git
 # && apt install -y nodejs \
 # && npm install -g wrangler purgecss postcss-cli autoprefixer cssnano
 RUN pip3 install --no-cache-dir gitblog2==${GITBLOG2_VERSION}
--- gitblog2/tests/test_e2e.py
+++ gitblog2/tests/test_e2e.py
@@ -1,39 +1,33 @@
 import filecmp
 import os
-import shutil
+from pathlib import Path
 import subprocess
 
-import pytest
-
-
-OUTPUT_DIR = "./public"
-GITBLOG_ARGS = [
-    "gitblog2",
-    "https://github.com/HenriTEL/gitblog2.git",
-    "--repo-subdir",
-    "example",
-    "--base-url",
-    "https://example.com",
+GITBLOG_CMD = [
+    "python3", "-m", "gitblog2.cli",
+    "--repo-subdir", "example",
+    "--base-url", "https://example.com",
     "--no-social",
+    "https://github.com/HenriTEL/gitblog2.git",
 ]
 
 
-@pytest.fixture(scope="session")
-def blog_dir():
-    subprocess.run(GITBLOG_ARGS, check=True)
-    yield OUTPUT_DIR
-    shutil.rmtree(OUTPUT_DIR)
+def test_content_match(tmp_path: Path):
+    cmd = GITBLOG_CMD + [str(tmp_path)]
+    subprocess.run(cmd, check=True)
 
-
-def test_content_match(blog_dir: str):
-    expected_content_dir = os.path.dirname(os.path.abspath(__file__)) + "/example_output"
+    expected_content_dir = Path(__file__).resolve().parent / "example_output"
     files = ["index.html", "tech/example.html", "tech/index.html"]
     (match, mismatch, errors) = filecmp.cmpfiles(
-        blog_dir, expected_content_dir, files
+        tmp_path, expected_content_dir, files
     )
+
     assert len(match) == len(files), f"mismatch: {mismatch}, errors: {errors}"
 
 
-def test_has_static_assets(blog_dir: str):
+def test_has_static_assets(tmp_path: Path):
+    cmd = GITBLOG_CMD + [str(tmp_path)]
+    subprocess.run(cmd, check=True)
+
     for path in ["media/favicon.svg", "media/icons.svg", "style.css"]:
-        assert os.path.exists(blog_dir + "/" + path)
+        assert os.path.exists(tmp_path / path)
